36392cdcccb554fd776447fd90688b6344dc7069
Oscar Theelen <oscartheelen@gmail.com>
2023-10-27 12:34:46 +0200

    finish js With AJAX!!!

--- WasteWatch/wwwroot/js/site.js
+++ WasteWatch/wwwroot/js/site.js
@@ -1,4 +1,35 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var canvas = document.getElementById("skiaCanvas");
+var ctx = canvas.getContext("2d");
 
-// Write your JavaScript code.
+var isDragging = false;
+var startX, startY, endX, endY;
+
+canvas.addEventListener("mousedown", function (e) {
+    isDragging = true;
+    startX = e.clientX - canvas.getBoundingClientRect().left;
+    startY = e.clientY - canvas.getBoundingClientRect().top;
+});
+
+canvas.addEventListener("mousemove", function (e) {
+    if (isDragging) {
+        endX = e.clientX - canvas.getBoundingClientRect().left;
+        endY = e.clientY - canvas.getBoundingClientRect().top;
+        drawSelectionBox();
+    }
+});
+
+canvas.addEventListener("mouseup", function () {
+    isDragging = false;
+    clearCanvas();
+});
+
+function drawSelectionBox() {
+    clearCanvas();
+    ctx.strokeStyle = "blue";
+    ctx.lineWidth = 2;
+    ctx.strokeRect(startX, startY, endX - startX, endY - startY);
+}
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
