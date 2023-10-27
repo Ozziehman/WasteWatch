6693c5c995bb9d74a66a33dc4db97c98159c29e7
Oscar Theelen <oscartheelen@gmail.com>
2023-10-27 11:01:52 +0200

    basic setup

---
+++ WasteWatch/Controllers/ImageController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WasteWatch.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WasteWatch.Controllers
+{
+	public class ImageController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+        [HttpPost]
+        public IActionResult UploadImage(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                var imageModel = new ImageModel
+                {
+                    ImageName = file.FileName,
+                    ImageData = new byte[file.Length]
+                };
+
+                using (var stream = new MemoryStream())
+                {
+                    file.CopyTo(stream);
+                    imageModel.ImageData = stream.ToArray();
+                }
+
+
+                return View("ImageDisplay", imageModel);
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ WasteWatch/Models/ImageModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WasteWatch.Models
+{
+    public class ImageModel
+    {
+        public string ImageName { get; set; }
+        public byte[] ImageData { get; set; }
+    }
+}
