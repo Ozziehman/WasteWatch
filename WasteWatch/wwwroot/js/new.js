2624cc3dcb6d45697b0ea6fa91f67f831859d15a
ProgrammerGhostPrK <2202630frederiks@zuyd.nl>
2023-11-01 22:19:04 +0100

    processedviewboxes

---
+++ WasteWatch/wwwroot/js/new.js
@@ -0,0 +1,63 @@
+﻿var boxes = [];
+var image = document.getElementById('Image');
+var body = document.getElementById('PageBody');
+var canvas = document.getElementById('Canvas');
+var ctx = canvas.getContext('2d');
+var startX, startY, endX, endY;
+var img = new Image();
+
+var boxFilled = false;
+var boxesFromDb = document.getElementById('boxesFromDb').value;
+
+if (canvas && image) {
+    // Set the image source and draw it on the canvas when it loads
+    img.src = image.src;
+    img.onload = function () {
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    };
+}
+
+body.addEventListener("mousemove", function (e) {
+    if (boxesFromDb != "" && boxes.length == 0 && boxFilled == false) {
+        boxes = JSON.parse(boxesFromDb);
+
+        boxFilled = true;
+        clearCanvas();
+        drawSelectionBox();
+        boxes.forEach(drawBox);
+    }
+});
+
+// Function to clear the canvas
+function clearCanvas() {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    boxes.forEach(drawBox);
+    console.log("Cleared canvas")
+}
+
+// Function to draw the selection box
+function drawSelectionBox() {
+    ctx.strokeStyle = 'blue';
+    ctx.lineWidth = 2;
+    let width = endX - startX;
+    let height = endY - startY;
+    ctx.strokeRect(startX, startY, width, height);
+}
+
+// Function to draw a box with a name
+function drawBox(box) {
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 2;
+    let width = box.endX - box.startX;
+    let height = box.endY - box.startY;
+    ctx.strokeRect(box.startX, box.startY, width, height);
+
+    ctx.fillStyle = 'white';
+    ctx.fillRect(box.startX, box.startY - 25, ctx.measureText(box.name).width + 10, 25);
+
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "black";
+    ctx.fillText(box.name, box.startX + 5, box.startY - 5);
+}
